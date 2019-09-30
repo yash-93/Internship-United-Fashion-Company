a23217b082bb9da39c2645339f2e99a2bdee8a50
yash-93 <yashdeepbachhas93@gmail.com>
2019-09-30 19:57:56 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     document.getElementById("header-nav").style.fontSize = "13px";
     document.getElementById("header-nav").style.height = "10vh";
+    document.getElementById("header-nav").classList.add("navvisible");
     document.getElementById("logo").style.height = "50px";
     document.getElementById("hide").classList.add("hide");
   } else {
     document.getElementById("header-nav").style.fontSize = "15px";
     document.getElementById("header-nav").style.height = "15vh";
+    document.getElementById("header-nav").classList.remove("navvisible");
     document.getElementById("logo").style.height = "75px";
     document.getElementById("hide").classList.remove("hide");
   }
